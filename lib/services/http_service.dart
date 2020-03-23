30f7c888a8f486a8956d160aead0a392910dbab3
Mohit <mohitagarwalferrari@gmail.com>
2020-03-23 13:20:26 +0530

    login url fixes

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -6,14 +6,10 @@ import 'package:http/http.dart' as http;
 class HttpService {
   final String hostelerURL = "http://localhost:9090/hosteler/get/1";
   Future<Hosteler> getHosteler() async {
-    print("I am here");
     http.Response res = await http.get(hostelerURL);
-    print("No I am here");
     if (res.statusCode == 200) {
-      print("OK");
       return Hosteler.fromJson(json.decode(res.body));
     } else {
-      print("Not OK");
       throw "Can't get Hostelers.";
     }
   }
