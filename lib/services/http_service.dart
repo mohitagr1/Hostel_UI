1529f6c1dbdbc6d1150fe91f8278405cce51f9c5
Mohit Agarwal <mohitagarwalferrari@gmail.com>
2020-03-26 02:00:30 +0530

    Update http_service.dart

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -4,11 +4,11 @@ import 'package:http/http.dart' as http;
 class HttpService {
   final String hostelerURL = "http://192.168.43.207:9090/hosteler/get/1";
   Future<Hosteler> getHosteler() async {
-    // //http.Response res = await http.get(hostelerURL);
-    // if (res.statusCode == 200) {
-    //   return Hosteler.fromJson(json.decode(res.body));
-    // } else {
-    //   //throw "Can't get Hostelers.";
-    // }
+    http.Response res = await http.get(hostelerURL);
+    if (res.statusCode == 200) {
+      return Hosteler.fromJson(json.decode(res.body));
+    } else {
+      throw "Can't get Hostelers.";
+    }
   }
 }
